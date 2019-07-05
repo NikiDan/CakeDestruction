61fd69cb7eb5e3126f8d2d5fa07700570002035e
Nikita <novakhudonosor@mail.ru>
2019-07-05 17:16:15 +0300

    Update 1.2. Hide was comleted successfully !

--- clear.js
+++ clear.js
@@ -1,3 +1,12 @@
 function clean() {
     document.getElementById('choice').value = ''
  }
+
+function hide() {
+    var x = document.getElementById("found");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
