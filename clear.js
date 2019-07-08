6fb1672ec312c7b92e8a6076b2f7b52e5eedaf95
Nikita <novakhudonosor@mail.ru>
2019-07-08 17:44:50 +0300

    rename complete

--- clear.js
+++ clear.js
@@ -1,26 +1,18 @@
 $(document).ready(function () {
-    $( ".hideB" ).click(function(){
+    $( ".hide" ).click(function(){
         $( ".content" ).slideToggle();
     });
 });
 
 function clean() {
-    document.getElementById('choice').value = '';
+    document.getElementById('input-form-down center').value = '';
  }
-// function cancel () {
-//        // var y = document.execCommand("undo");
-//        var  z = document.getElementById('choice');
-//      if (z.value = 'Благоприятный, трансцендентальный') {
-//          z.value = '';
-//      }
-//             z.value = 'Благоприятный, трансцендентальный';
-//
-// }
+
 var arr = ["", "Моноблок","Благоприятный, трансцендентальный"];
 function reset() {
     var i = document.getElementById('input1');
     var k = document.getElementById('monoblock');
-    var c = document.getElementById('choice');
+    var c = document.getElementById('input-form-down center');
     i.value = arr[0];
     k.value = arr[1];
     c.value = arr[2];
