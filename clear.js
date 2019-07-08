fb1b055ff68cd2cb20a1811c6dba59fe3cdd795a
Nikita <novakhudonosor@mail.ru>
2019-07-08 16:32:56 +0300

    update 1.3

--- clear.js
+++ clear.js
@@ -7,12 +7,22 @@ $(document).ready(function () {
 function clean() {
     document.getElementById('choice').value = '';
  }
-function cancel () {
-    var y = document.execCommand("undo");
-    // var x = document.getElementById("found");
-    // if (x.style.display === "block") {
-    //     x.style.display = "none";
-    // } else {
-    //     x.style.display = "block";
-    // }
+// function cancel () {
+//        // var y = document.execCommand("undo");
+//        var  z = document.getElementById('choice');
+//      if (z.value = 'Благоприятный, трансцендентальный') {
+//          z.value = '';
+//      }
+//             z.value = 'Благоприятный, трансцендентальный';
+//
+// }
+var arr = ["", "Моноблок","Благоприятный, трансцендентальный"];
+function reset() {
+    var i = document.getElementById('input1');
+    var k = document.getElementById('monoblock');
+    var c = document.getElementById('choice');
+    i.value = arr[0];
+    k.value = arr[1];
+    c.value = arr[2];
 }
+
