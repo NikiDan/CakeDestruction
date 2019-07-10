005b27836a5e6fee32e62738cbb1e64c959e3097
Nikita <novakhudonosor@mail.ru>
2019-07-10 15:32:25 +0300

    update1.4

--- clear.js
+++ clear.js
@@ -1,20 +1,34 @@
 $(document).ready(function () {
-    $( ".hide" ).click(function(){
-        $( ".content" ).slideToggle();
+    $(".hide").click(function () {
+        $(".content-rectangle").slideToggle();
     });
 });
 
 function clean() {
-    document.getElementById('input-form-down center').value = '';
+    document.getElementById('input-form-down-rectangle').value = '';
  }
 
-var arr = ["", "Моноблок","Благоприятный, трансцендентальный"];
+var arr = [ "",
+            "Моноблок",
+            "Благоприятный, трансцендентальный"];
 function reset() {
-    var emptyFillingInput = document.getElementById('input1');
-    var monoblock = document.getElementById('monoblock');
-    var exodusField = document.getElementById('input-form-down');
-    emptyFillingInput.value = arr[0];
-    monoblock.value = arr[1];
-    exodusField.value = arr[2];
+
+    var fillingInput = document.getElementById('input-form-up-rectangle');
+    var formFactor = document.getElementById('formFactor-rectangle');
+    var exodusField = document.getElementById('input-form-down-rectangle');
+
+    if (document.getElementById('yes')) {
+        fillingInput.value = arr[0];
+        formFactor.value= arr[1];
+        exodusField.value = arr[2];
+     }
+     else {
+        document.getElementById('no');
+     }
+}
+function transfer() {
+   document.getElementById('filling-field-square').innerHTML = document.getElementById('input-form-up-rectangle').value;
+   document.getElementById('formFactor-field-square').innerHTML = document.getElementById('formFactor-rectangle').value;
+   document.getElementById('exodus-field-square').innerHTML = document.getElementById('input-form-down-rectangle').value;
 }
 
