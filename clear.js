43d333718d6ac1202ae07dd30c0a3ab07ed53ef5
Nikita <novakhudonosor@mail.ru>
2019-07-11 13:07:39 +0300

    update 1.5

--- clear.js
+++ clear.js
@@ -1,17 +1,25 @@
 $(document).ready(function () {
-    $(".hide").click(function () {
-        $(".content-rectangle").slideToggle();
+
+    $(".hide-content").click(function () {
+
+        $(this).closest(".cap").siblings(".content").slideToggle();
+
+        // if ($(this).hasClass("hide-rectangle")) {
+        //     $(".content-rectangle").slideToggle();
+        // } else {
+        //     $(".content-square").slideToggle();
+        // }
     });
 });
 
-function clean() {
+function cleanRectangle() {
     document.getElementById('input-form-down-rectangle').value = '';
- }
+}
 
 var arr = [ "",
             "Моноблок",
             "Благоприятный, трансцендентальный"];
-function reset() {
+function resetRectangle() {
 
     var fillingInput = document.getElementById('input-form-up-rectangle');
     var formFactor = document.getElementById('formFactor-rectangle');
@@ -31,4 +39,8 @@ function transfer() {
    document.getElementById('formFactor-field-square').innerHTML = document.getElementById('formFactor-rectangle').value;
    document.getElementById('exodus-field-square').innerHTML = document.getElementById('input-form-down-rectangle').value;
 }
-
+function cleanSquare() {
+    document.getElementById('exodus-field-square').innerHTML = arr[0];
+    document.getElementById('formFactor-field-square').innerHTML = arr[0];
+    document.getElementById('filling-field-square').innerHTML = arr[0];
+}
