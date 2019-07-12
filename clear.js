9f6a6c7f7cb715a72daff707beb880000fce82a9
Nikita <novakhudonosor@mail.ru>
2019-07-12 14:11:51 +0300

    Update 1.7

--- clear.js
+++ clear.js
@@ -17,6 +17,7 @@ $(document).ready(function () {
         $("#select-file").click();
 
     })
+
 });
 
 function cleanRectangle() {
@@ -26,6 +27,26 @@ function cleanRectangle() {
 var arr = [ "",
             "Моноблок",
             "Благоприятный, трансцендентальный"];
+
+function transfer() {
+   document.getElementById('filling-field-square').innerHTML = document.getElementById('input-form-up-rectangle').value;
+   document.getElementById('formFactor-field-square').innerHTML = document.getElementById('formFactor-rectangle').value;
+   document.getElementById('exodus-field-square').innerHTML = document.getElementById('input-form-down-rectangle').value;
+}
+function cleanSquare() {
+    document.getElementById('exodus-field-square').innerHTML = arr[0];
+    document.getElementById('formFactor-field-square').innerHTML = arr[0];
+    document.getElementById('filling-field-square').innerHTML = arr[0];
+}
+
+// var span = document.getElementsByClassName("close")[0];
+
+function openModal () {
+
+    document.getElementById('confirm').style.display = "block";
+
+}
+
 function resetRectangle() {
 
     var fillingInput = document.getElementById('input-form-up-rectangle');
@@ -36,20 +57,42 @@ function resetRectangle() {
         fillingInput.value = arr[0];
         formFactor.value= arr[1];
         exodusField.value = arr[2];
-        document.getElementById('confirm').display="none";
-     }
-     else {
-        document.getElementById('no');
-        document.getElementById('confirm').display="none";
-     }
+        document.getElementById('confirm').style.display="none";
+    }
 }
-function transfer() {
-   document.getElementById('filling-field-square').innerHTML = document.getElementById('input-form-up-rectangle').value;
-   document.getElementById('formFactor-field-square').innerHTML = document.getElementById('formFactor-rectangle').value;
-   document.getElementById('exodus-field-square').innerHTML = document.getElementById('input-form-down-rectangle').value;
+
+function closeModal() {
+
+    document.getElementById('confirm').style.display="none";
+
 }
-function cleanSquare() {
-    document.getElementById('exodus-field-square').innerHTML = arr[0];
-    document.getElementById('formFactor-field-square').innerHTML = arr[0];
-    document.getElementById('filling-field-square').innerHTML = arr[0];
+function transferData() {
+    var data = document.getElementById('select-file');
+    var text = "";
+    if ('files' in data){
+        if (data.files.length == 0){
+            text = "Вам необходимо выбрать хотя бы один файл !";
+        }   else{
+            for (var i=0; i<data.files.length; i++){
+                text +="<br><strong>" + (i+1) + " .files</strong><br>";
+                var file = data.files[i];
+                if('name' in file){
+                    text += "name: " + file.name + "<br>";
+                }
+                if('size' in file){
+                    text +="size: " +file.size + " bytes<br>"
+                }
+            }
+        }
+    }
+    else{
+        if (data.value ==""){
+            text += "Вам необходимо выбрать хотя бы один файл !"
+        }
+        else{
+            text += "Этот файл не поддерживается вашим браузером !";
+            text +="<br> Часть выбранного файла: " + data.value;
+        }
+    }
+    document.getElementById('data-file').innerHTML = text;
 }
